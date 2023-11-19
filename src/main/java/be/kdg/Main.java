3398a4432756516d5c44fb91d9e2a7760aa05e8e
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-11-20 00:17:35 +0100

    fix layout

--- src/main/java/be/kdg/Main.java
+++ src/main/java/be/kdg/Main.java
@@ -9,17 +9,20 @@ public class Main {
         GameBoard gameBoard = new GameBoard();
 
         // TODO: move all printing to presentation layer
+        // TODO: center value within cell
         for (Figure[] row : gameBoard.getGrid().getSlots())
         {
+            System.out.println("-".repeat(108));
             for (Figure figure : row)
             {
                 if (figure == null) {
-                    System.out.printf("%10s", 'X');
+                    System.out.printf("%10s |", 'X');
                     continue;
                 }
-                System.out.printf("%10s ", figure);
+                System.out.printf("%10s |", figure);
             }
             System.out.print("\n");
         }
+        System.out.println("-".repeat(108));
     }
 }
