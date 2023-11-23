c6c491d1acbe2f42a6b316e69f9cdbac9a89128a
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-11-23 17:54:35 +0100

    test MoveInputManager

--- src/main/java/be/kdg/tablut/presentation/ascii/Tablut.java
+++ src/main/java/be/kdg/tablut/presentation/ascii/Tablut.java
@@ -1,8 +1,10 @@
 package be.kdg.tablut.presentation.ascii;
 
+import be.kdg.tablut.domain.board.BoardPosition;
 import be.kdg.tablut.domain.game.Game;
 import be.kdg.tablut.domain.game.MoveTurn;
 import be.kdg.tablut.domain.player.Player;
+import be.kdg.tablut.presentation.ascii.input.MoveInputManager;
 import be.kdg.tablut.presentation.ascii.output.BoardDisplayManager;
 
 public class Tablut {
@@ -15,6 +17,7 @@ public class Tablut {
 
     public void playGame() {
         BoardDisplayManager.printGameBoard(game.getBoard());
-    }
 
+        BoardPosition playerInput = MoveInputManager.takeMoveInput();
+    }
 }
