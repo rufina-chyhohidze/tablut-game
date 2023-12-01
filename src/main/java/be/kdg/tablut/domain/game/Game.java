ea793144b52b793e49908c5bd59baae027acf127
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-12-01 15:25:56 +0100

    add comment

--- src/main/java/be/kdg/tablut/domain/game/Game.java
+++ src/main/java/be/kdg/tablut/domain/game/Game.java
@@ -18,6 +18,9 @@ public class Game {
     private final Player playerBlack;
 
     public Game(Player playerWhite, Player playerBlack) {
+        // if you want to see the end-game screens
+        // Use This line instead
+//        this.board = BoardFactory.getWhiteWonGrid();
         this.board = BoardFactory.getDefaultBoard();
         this.moveTurn = MoveTurn.WHITE;
         this.startedAt = LocalDateTime.now();
