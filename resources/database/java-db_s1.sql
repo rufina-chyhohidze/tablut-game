4f9667e3e18f82dbc9789049423d7b206671378f
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-12-04 15:06:58 +0100

    wip DB

--- resources/database/java-db_s1.sql
+++ resources/database/java-db_s1.sql
@@ -1,5 +1,6 @@
-CREATE TABLE IF NOT EXISTS player_scores (
-     player_name varchar(255),
-     score numeric(999) default 0,
-     game_date timestamp
+CREATE TABLE IF NOT EXISTS INT_player_scores (
+     INT_id SERIAL primary key,
+     INT_player_name varchar(20),
+     INT_score numeric(10) default 0,
+     INT_game_date timestamp
 );
