dbb8794a4d4c96b6f5478511ce905e241ad65602
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-12-11 11:22:42 +0100

    implement restart

---
+++ src/main/java/be/kdg/tablut/presentation/ascii/output/EndGameCommandsManager.java
@@ -0,0 +1,9 @@
+package be.kdg.tablut.presentation.ascii.output;
+
+public class EndGameCommandsManager {
+
+    public static void printEndGameCommands() {
+        // TODO: implement me
+    }
+
+}
