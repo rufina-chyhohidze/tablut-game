2ef929bee362273ba3dde3c83e78ba90d1ef4d57
Vlad Ivanov <vlad.ivanov@student.kdg.be>
2023-12-18 14:03:01 +0200

    Implemented output of table "top 5" after the game (Aligned)

--- src/main/java/be/kdg/tablut/data/postgres/repository/PlayerStatsPostgresRepository.java
+++ src/main/java/be/kdg/tablut/data/postgres/repository/PlayerStatsPostgresRepository.java
@@ -6,8 +6,13 @@ import be.kdg.tablut.domain.game.ScoreManager;
 import be.kdg.tablut.domain.player.PlayerLeaderboardStats;
 import be.kdg.tablut.domain.player.repository.IPlayerStatsRepository;
 import be.kdg.tablut.domain.player.Player;
-
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.format.DateTimeFormatter;
+
+
+
 
 public class PlayerStatsPostgresRepository implements IPlayerStatsRepository {
     // TODO: implement me
@@ -65,9 +70,35 @@ public class PlayerStatsPostgresRepository implements IPlayerStatsRepository {
 
     @Override
     public PlayerLeaderboardStats[] getPlayerLeaderboardStatus(String username) {
-        return new PlayerLeaderboardStats[0];
+        List<PlayerLeaderboardStats> leaderboardStatsList = new ArrayList<>();
+
+        String sqlQuery = "SELECT INT_player_name, INT_score, INT_game_date FROM INT_player_scores ORDER BY INT_score DESC LIMIT 5";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                String playerName = resultSet.getString("INT_player_name");
+                double score = resultSet.getDouble("INT_score");
+                Timestamp gameDate = resultSet.getTimestamp("INT_game_date");
+
+                // Создайте объект PlayerLeaderboardStats и добавьте его в список
+                PlayerLeaderboardStats leaderboardStats = new PlayerLeaderboardStats();
+                leaderboardStatsList.add(leaderboardStats);
 
 
+                System.out.printf("Player: %-15s, Score: %-5.2f, Game Date: %s%n", playerName, score, gameDate.toLocalDateTime().format(formatter));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Обработка исключений, связанных с базой данных
+        }
+
+        // Преобразуйте список в массив и верните его
+        return leaderboardStatsList.toArray(new PlayerLeaderboardStats[0]);
     }
 
 }
+
+
